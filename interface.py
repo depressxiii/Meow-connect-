a7cb18f0be0bb4bcce8b439aead9853529f1a17d
depressxiii <144928765+depressxiii@users.noreply.github.com>
2023-10-20 16:28:28 +0200

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import server
+import pygame
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+
+# Fonction qui sera appelée lorsque le bouton est cliqué
+def bouton_clique():
+    print("Le serveur est sur écoute... Patientez un moment")
+    server.receive()
+
+# Créer une fenêtre
+fenetre = tk.Tk()
+fenetre.geometry("600x600")  # Définir la taille de la fenêtre
+
+# Personnaliser les couleurs
+fenetre.configure(bg="#F3F3F3")  # Couleur de fond
+
+# Initialiser Pygame pour la musique
+pygame.mixer.init()
+pygame.mixer.music.load("never.mp3")  # Remplacez "votre_musique.mp3" par le chemin de votre fichier audio
+pygame.mixer.music.set_volume(0.5)  # Réglez le volume (facultatif)
+pygame.mixer.music.play(-1)  # "-1" signifie que la musique se répétera en boucle
+
+# Afficher un GIF au milieu
+image = Image.open("votre_gif.gif")  # Remplacez "votre_gif.gif" par le chemin de votre GIF
+photo = ImageTk.PhotoImage(image)
+gif_label = tk.Label(fenetre, image=photo, bg="#F3F3F3")
+gif_label.image = photo
+gif_label.place(relx=0.5, rely=0.5, anchor="center")
+
+# Personnaliser le bouton
+cadre = tk.Frame(fenetre, bg="#E0E0E0")  # Couleur de fond du cadre
+cadre.place(relx=0.5, rely=0.9, anchor="s")  # Déplacer vers le bas
+
+bouton = tk.Button(cadre, text="Cliquez-moi", command=bouton_clique, bg="#4CAF50", fg="white", font=("Helvetica", 16), bd=5, relief=tk.RAISED)
+bouton.pack(padx=10, pady=10)
+
+# Créer une étiquette vide
+label = tk.Label(fenetre, text="")
+label.pack()
+
+# Lancer la boucle principale de l'interface graphique
+fenetre.mainloop()
